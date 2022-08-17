377b33a5783101b73cc0b5a8c0099a51b09a2df0
Sergey Misko <sergik.m@outlook.com>
2022-08-17 20:08:36 +0500

    добавил метод GetStats() для вывода итоговой информации

--- MyEBookReader/Program.cs
+++ MyEBookReader/Program.cs
@@ -2,11 +2,13 @@
 using System.Threading.Tasks;
 using System.Threading;
 using System.Net;
+using System.Text;
 
 namespace MyEBookReader
 {
     class Program
     {
+        private static string theEBook;
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
@@ -16,7 +18,7 @@ namespace MyEBookReader
             WebClient wc = new WebClient();
             wc.DownloadStringCompleted += (s, eArgs) =>
              {
-                 var theEBook = eArgs.Result;
+                 theEBook = eArgs.Result;
                  Console.WriteLine("Download complete.");
                  GetStats();
              };
@@ -25,6 +27,29 @@ namespace MyEBookReader
             // не посещали данный сайт, поскольку при первом его посещении появляется
             // окно с сообщением, предотвращающее нормальное выполнение кода.
             wc.DownloadStringAsync(new Uri("http://www.gutenberg.org/files/98/98-8.txt"));
+        }
+        static void GetStats()
+        {
+            // Получить слова из электронной книги.
+            string[] words = theEBook.Split(new char[]
+            { ' ', '\u000A', ',', '.', ';', ':', '-', '?', '/' },
+            StringSplitOptions.RemoveEmptyEntries);
+            // Найти 10 наиболее часто встречающихся слов.
+            string[] tenMostCommon = FindTenMostCommon(words);
+            // Получить самое длинное слово.
+            string longestWord = FindLongestWord(words);
+            // Когджа все задачи завершены, построить строку,
+            // показывающую всю статистику в окне сообщений.
+            StringBuilder bookStats = new StringBuilder("Ten Most Common Words are:\n");
+            foreach (string s in tenMostCommon)
+            {
+                bookStats.Append(s);
+            }
+            // Самое длинное слово.
+            bookStats.AppendFormat("Longest word is: {0}", longestWord);
+            bookStats.AppendLine();
+            // Информация о книге.
+            Console.WriteLine(bookStats.ToString(),"Book info");
         }
     }
 }

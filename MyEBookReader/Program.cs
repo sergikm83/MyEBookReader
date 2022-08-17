3691cd089b0936e95cbb4a1ebf06615920bd5d82
Sergey Misko <sergik.m@outlook.com>
2022-08-17 19:56:49 +0500

    добавил метод GetBook()

--- MyEBookReader/Program.cs
+++ MyEBookReader/Program.cs
@@ -11,5 +11,20 @@ namespace MyEBookReader
         {
             Console.WriteLine("Hello World!");
         }
+        static void GetBook()
+        {
+            WebClient wc = new WebClient();
+            wc.DownloadStringCompleted += (s, eArgs) =>
+             {
+                 var theEBook = eArgs.Result;
+                 Console.WriteLine("Download complete.");
+                 GetStats();
+             };
+            // Загрузить электронную книгу Чарльза Диккенса "A RTale of Two Cities".
+            // Может понадобиться двукратное выполнение этого кода, если ранее вы
+            // не посещали данный сайт, поскольку при первом его посещении появляется
+            // окно с сообщением, предотвращающее нормальное выполнение кода.
+            wc.DownloadStringAsync(new Uri("http://www.gutenberg.org/files/98/98-8.txt"));
+        }
     }
 }
